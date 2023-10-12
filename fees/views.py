b712e2e19a4884fb6398421784425af0ee15f6d3
itsgud <92721403+itsgud@users.noreply.github.com>
2023-10-12 13:07:46 +0530

    Delete fees directory

--- fees/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def fees_django(request):
-    return render(request,'fees/feesone.html',{'title':'fees for django','cname':'Django','charge':300})
